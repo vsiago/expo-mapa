5c896605d86fe4c984c5e633697e6d3683633769
vsiago <vsiago23@gmail.com>
2023-06-15 19:52:21 -0300

    pagina avaliacao iniciada

--- src/pages/Avaliacao/Avaliacao.jsx
+++ src/pages/Avaliacao/Avaliacao.jsx
@@ -4,15 +4,36 @@ import { Link } from 'react-router-dom';
 
 export default function Avaliacao() {
   return (
-    <div className="avaliacao-container">
-      <h1 className="fade-in">Pagina Avaliacao</h1>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/home">voltar</Link>
-          </li>
-        </ul>
-      </nav>
-    </div>
+    <main className="avaliacao-container">
+      <header>
+        <h1 className="fade-in">
+          <img
+            className="logoExpo"
+            src="./img/logo-expo-2023.svg"
+            width={200}
+            alt="Logo Expo 2023"
+          />
+        </h1>
+        <h1 className="titleOpiniao">Dê sua opinião sobre a Expo</h1>
+        <p className="subTitleOpniao">
+          Sua resposta é muito importante para nós
+        </p>
+        <Link to="/home">
+          <img className="btnVoltar" src="./img/btnVoltar.png" alt="" />
+        </Link>
+      </header>
+      <section>
+        <div className="cardsAvaliacao">
+          <div className="cardSeguraca card"></div>
+          <div className="cardAlimentacao card"></div>
+          <div className="cardOrganizacao card"></div>
+          <div className="cardAtracao card"></div>
+        </div>
+        <button className="btnEnviarAvaliacao">Enviar avaliação</button>
+      </section>
+      <footer>
+        <img className="scLogos" src="../img/scLogos.png" alt="" />
+      </footer>
+    </main>
   );
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import Header from '../components/Header';
+import Header from '../../components/Header';
 import './home.css';
 
 const Home = () => {
