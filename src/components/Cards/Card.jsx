e43a34e30f4fb49d419b5be25236ff68d4363acb
vsiago <vsiago23@gmail.com>
2023-06-17 18:57:20 -0300

    interacao nas carinhas iniciada

--- src/components/Cards/Card.jsx
+++ src/components/Cards/Card.jsx
@@ -1,16 +1,33 @@
 import React from 'react';
 import './card.css';
+import { useState } from 'react';
 
 export default function Card({ TipoDeArea }) {
+  const [activeButton, setActiveButton] = useState('');
+
+  const handleButtonClick = (button) => {
+    setActiveButton(button);
+  };
+
   return (
     <div className="cardContainer">
       <div className="titleCardContainer">
         <h3>{TipoDeArea}</h3>
       </div>
-      <div className="icon">
-        <img src="./img/iconsCaras/muitoRuimIcon.png" alt="" />
-        <p>Muito ruim</p>
+
+      <div onClick={() => handleButtonClick('muitoRuim')} className="icon">
+        <img
+          className={
+            activeButton === 'muitoRuim' ? 'muitoRuimActive iconRuimActive' : ''
+          }
+          src="./img/iconsCaras/muitoRuimIcon.png"
+          alt=""
+        />
+        <p className={activeButton === 'muitoRuim' ? 'TextRuimActive' : ''}>
+          Muito ruim
+        </p>
       </div>
+
       <div className="icon">
         <img src="./img/iconsCaras/ruimIcon.png" alt="" />
         <p>Ruim</p>
