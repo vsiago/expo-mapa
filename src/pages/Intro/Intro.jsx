ad76d92164e1da1829c8ce9506095a1e52d5d803
vsiago <vsiago23@gmail.com>
2023-06-19 07:29:19 -0300

    Finalizado MPV

--- src/pages/Intro/Intro.jsx
+++ src/pages/Intro/Intro.jsx
@@ -5,7 +5,8 @@ const Intro = () => {
   return (
     // Estrutura visual da sua intro
     <div className="intro-container">
-      <img src="./img/logo-expo-2023.svg" />
+      <img src="./img/logo-expo-2023.svg" width={230} />
+      <p className='textMapaInterativo'>MAPA INTERATIVO</p>
     </div>
   );
 };
