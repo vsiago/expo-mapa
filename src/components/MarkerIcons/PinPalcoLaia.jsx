cbb8a3b6b67ed6bc7b0ff2baadf44556982afa4f
vsiago <vsiago23@gmail.com>
2023-06-26 21:19:31 -0300

    atualizacao

--- src/components/MarkerIcons/PinPalcoLaia.jsx
+++ src/components/MarkerIcons/PinPalcoLaia.jsx
@@ -1,4 +1,5 @@
 import { icon } from 'leaflet';
+import { useState } from 'react';
 import { Marker, Popup } from 'react-leaflet';
 import pinPalcoLaiaIcon from '/img/iconsCaras/muitoRuimIcon.png';
 import './pinpalcolaia.css';
@@ -7,40 +8,64 @@ import PalcoLaia from '../../ModelSetoresExpo/PalcoLaia';
 const PinPalcoLaia = ({ latitude, longitude }) => {
   const customIconPalcoLaia = icon({
     iconUrl: pinPalcoLaiaIcon,
-    iconSize: [50, 50], // Defina o tamanho do ícone
+    iconSize: [50, 50],
   });
 
   const coordPinPalcoLaia = [latitude, longitude];
 
+  const [itemSelecionado, setItemSelecionado] = useState(null);
+
+  const handleItemClick = (item) => {
+    setItemSelecionado(item);
+  };
+
+  const renderAtracao = () => {
+    if (itemSelecionado) {
+      return (
+        <header>
+          <div className='img-container'>
+            <img src={PalcoLaia.URLImg} alt='' />
+          </div>
+          <div className='containerIntro'>
+            <h1>{itemSelecionado.name}</h1>
+            <p>{itemSelecionado.description}</p>
+          </div>
+        </header>
+      );
+    } else {
+      return (
+        <header>
+          <div className='img-container'>
+            <img src={PalcoLaia.URLImg} alt='' />
+          </div>
+          <div className='containerIntro'>
+            <h1>{PalcoLaia.name}</h1>
+            <p>{PalcoLaia.description}</p>
+          </div>
+        </header>
+      );
+    }
+  };
 
   return (
-    <Marker
-      position={coordPinPalcoLaia}
-      icon={customIconPalcoLaia}
-    >
-        <Popup autoPan={false} >
-          <main>
-            <header>
-              <div className='img-container'>
-                <img src={PalcoLaia.URLImg} alt="" />
-              </div>
-              <div className='containerIntro'>
-                <h1>{PalcoLaia.name}</h1>
-                <p>{PalcoLaia.description}</p>
-              </div>
-            </header>
-            <footer>
-              <nav>
+    <Marker position={coordPinPalcoLaia} icon={customIconPalcoLaia}>
+      <Popup autoPan={false}>
+        <main>
+          {renderAtracao()}
+          <footer>
+            <nav>
               <h3>TODAS AS ATRAÇÕES:</h3>
-                <ul className='navAtracoes'>
-                  {PalcoLaia.atracoes.map((atracao) => (
-                    <li><p key={atracao.id} name={atracao.name}>{atracao.name}</p></li>
-                  ))}
-                </ul>
-              </nav>
-            </footer>
-          </main>
-        </Popup>
+              <ul className='navAtracoes'>
+                {PalcoLaia.atracoes.map((atracao) => (
+                  <li key={atracao.id}>
+                    <p onClick={() => handleItemClick(atracao)}>{atracao.name}</p>
+                  </li>
+                ))}
+              </ul>
+            </nav>
+          </footer>
+        </main>
+      </Popup>
     </Marker>
   );
 };
