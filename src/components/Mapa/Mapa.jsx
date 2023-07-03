a8dda97c80df195ceeb8d233917d894a21ee36ad
vsiago <vsiago23@gmail.com>
2023-07-03 08:45:42 -0300

    pins completos

--- src/components/Mapa/Mapa.jsx
+++ src/components/Mapa/Mapa.jsx
@@ -8,6 +8,10 @@ import PinCamaroteGreyGoose from '../MarkerIcons/PinGreyGoose';
 import PinRodeio from '../MarkerIcons/PinRodeio';
 import PinParque from '../MarkerIcons/PinParque';
 import PinFazendinha from '../MarkerIcons/PinFazendinha';
+import PinArenaGame from '../MarkerIcons/PinArenaGame';
+import PinArenaEducacao from '../MarkerIcons/PinArenaEducacao';
+import PinPracaAlimentacao from '../MarkerIcons/PinAlimentacao';
+import PinMariaBonita from '../MarkerIcons/PinMariaBonita';
 
 // CONSTRUCAO DO MAPA BASEADO NA IMAGEM
 const Map = () => {
@@ -63,14 +67,14 @@ const Map = () => {
         <PinPalcoLaia latitude={'65.000'} longitude={'-80.000'} />
         {/* <PinPalcoLaia latitude={'46.000'} longitude={'-70.000'} /> */}
         <PinParque latitude={'32.000'} longitude={'105.000'} />
-        <PinParque latitude={'-26.000'} longitude={'55.000'} /> // Praca de alimentacao
-        <PinParque latitude={'-15.000'} longitude={'136.000'} /> // Arena Game
-        <PinParque latitude={'-39.000'} longitude={'170.000'} /> // Arena Educacao
+        <PinPracaAlimentacao latitude={'-26.000'} longitude={'55.000'} /> // Praca de alimentacao
+        <PinArenaGame latitude={'-15.000'} longitude={'136.000'} /> // Arena Game
+        <PinArenaEducacao latitude={'-39.000'} longitude={'170.000'} /> // Arena Educacao
         <PinFazendinha latitude={'5.000'} longitude={'203.000'} />
         <PinRodeio latitude={'36.000'} longitude={'-110.000'} />
         <PinCamaroteGreyGoose latitude={'70.000'} longitude={'-30.000'} />
         <PinPalcoQuiva latitude={'-77.000'} longitude={'104.000'} />
-        <PinPalcoLaia latitude={'6.000'} longitude={'-89.000'} /> // Maria Bonita
+        <PinMariaBonita latitude={'6.000'} longitude={'-89.000'} /> // Maria Bonita
       </MapContainer>
     </div>
   );
